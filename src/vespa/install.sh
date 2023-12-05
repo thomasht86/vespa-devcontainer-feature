50e672a6b3a4e2be12d742613ef7306b155fc210
thomasht86 <thomas.h.thoresen@gmail.com>
2023-12-05 10:02:40 +0100

    test

--- src/vespa/install.sh
+++ src/vespa/install.sh
@@ -3,20 +3,21 @@ set -e
 set -x
 
 echo "Activating feature 'Vespa'..."
+
 # Fetching the latest Vespa release version
 echo 'Fetching the latest Vespa release version...'
-curl -s https://api.github.com/repos/vespa-engine/vespa/releases/latest | jq -r '.tag_name' | sed 's/v//' > /tmp/vespa_version
+VESPA_VERSION=$(curl -s https://api.github.com/repos/vespa-engine/vespa/releases/latest | jq -r '.tag_name' | sed 's/v//')
+echo "Latest Vespa version: $VESPA_VERSION"
 
-# Storing the version in a variable
-VERSION=$(cat /tmp/vespa_version)
-echo "Latest Vespa version: $VERSION"
+# Ensuring that the download directory exists
+DOWNLOAD_DIR="/tmp/vespa-cli-${VESPA_VERSION}"
+mkdir -p "$DOWNLOAD_DIR"
 
 # Downloading and extracting the Vespa CLI
 echo 'Downloading and extracting the Vespa CLI...'
-curl -fsSL "https://github.com/vespa-engine/vespa/releases/download/v${VERSION}/vespa-cli_${VERSION}_linux_amd64.tar.gz" | tar -zxf -
+curl -fsSL "https://github.com/vespa-engine/vespa/releases/download/v${VESPA_VERSION}/vespa-cli_${VESPA_VERSION}_linux_amd64.tar.gz" | tar -zxf - -C "$DOWNLOAD_DIR"
 
 # Making the Vespa CLI executable available system-wide
 echo 'Making the Vespa CLI executable available system-wide...'
-ln -sf "$(pwd)/vespa-cli_${VERSION}_linux_amd64/bin/vespa" /usr/local/bin/vespa
-
+ln -sf "$DOWNLOAD_DIR/vespa-cli_${VESPA_VERSION}_linux_amd64/bin/vespa" /usr/local/bin/vespa
 chmod +x /usr/local/bin/vespa
