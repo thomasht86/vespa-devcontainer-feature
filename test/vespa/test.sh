7345f03090ed78f14ddd622a698fa467874c3cfd
thomasht86 <thomas.h.thoresen@gmail.com>
2023-12-05 10:05:43 +0100

    fix test

--- test/vespa/test.sh
+++ test/vespa/test.sh
@@ -8,11 +8,4 @@ source dev-container-features-test-lib
 # Check if 'vespa' command is available
 check "vespa command is available" which vespa
 
-# Set vespa configuration before getting it
-# Replace 'your-config' with your actual configuration
-check "set vespa configuration" vespa config set your-config
-
-# Now get the configuration
-check "get vespa configuration" vespa config get
-
 reportResults
